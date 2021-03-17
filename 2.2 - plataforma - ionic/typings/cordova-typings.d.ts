c7cd80d7c05c4c650b4560988f014adb9f0c746a
Marcelocoelhop <=>
2021-03-17 13:48:22 -0300

    feature: Change app platform to ionic

---
+++ 2.2 - plataforma - ionic/typings/cordova-typings.d.ts
@@ -0,0 +1,11 @@
+/// <reference path="..\.vscode\typings\ionic\ionic.d.ts"/>
+/// <reference path="..\.vscode\typings\angularjs\angular.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova-ionic\plugins\keyboard.d.ts"/>
+/// <reference path="..\.vscode\typings\jquery\jquery.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\cordova.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\StatusBar.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Splashscreen.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\BatteryStatus.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Contacts.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\DeviceOrientation.d.ts"/>
+/// <reference path="..\.vscode\typings\cordova\plugins\Vibration.d.ts"/>
